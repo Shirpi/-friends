03eb3c0991e0f320b7bc7c777e508695ee05960c
Shirpi <shirpi.cs13@bitsathy.ac.in>
2016-08-08 12:40:03 +0530

    Update Program8.c

--- Program8.c
+++ Program8.c
@@ -4,10 +4,15 @@ int main()
  int n, i, sum = 0;
  printf("Enter a positive integer: ");
  scanf("%d",&n);
+ if(n>=0)
+ {
  for(i=1; i <= n; ++i)
-  {
-  sum += i;   
-  }
-printf("Sum = %d",sum);
+ {
+ sum += i;   
+ }
+ printf("Sum = %d",sum);
+}
+else
+ printf("%d is not a natural nmber",n)
 return 0;
 }
